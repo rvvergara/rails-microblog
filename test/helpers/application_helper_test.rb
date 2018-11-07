e900b5905d86155d9a74cf6851cc00e9f6758343
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2018-11-07 17:06:27 +0800

    Add users signup page w/ relevant tests and routes

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -4,5 +4,6 @@ class ApplicationHelperTest < ActionView::TestCase
   test "full title helper" do
     assert_equal full_title, "Rails Microblog"
     assert_equal full_title("Help"), "Help | Rails Microblog"
+    assert_equal full_title("Signup"), "Signup | Rails Microblog"
   end
 end

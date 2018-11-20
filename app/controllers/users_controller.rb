7d310f71f2e1b5c9205508d386e219957c21974d
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2018-11-20 18:12:53 +0800

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,8 +20,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to MicroBlog!!"
-      redirect_to user_url(@user)
+      log_in(@user)
+      flash[:success] = "Welcome to MicroBlog, #{@user.name}!!"
+      redirect_to @user
     else
       render 'new'
     end

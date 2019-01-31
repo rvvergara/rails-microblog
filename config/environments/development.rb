4ee58295f616c54fee9c1e46179daf047acb2eb6
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-01-31 15:35:05 +0800

    Incorporate smtp to mail sending in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,11 +33,11 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.delivery_method = :test
+  config.action_mailer.delivery_method = :smtp
 
   host = 'localhost:3000'
 
-  config.action_mailer.default_url_options = {host: host, protocol: 'http'}
+  config.action_mailer.default_url_options = {host: host}
 
   config.action_mailer.perform_caching = false
 
@@ -64,4 +64,15 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
+
 end
